3e0fcede8937577ae7221ab4c81b558316acea63
maechabin <mail@chab.in>
2015-09-02 01:49:14 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+var Dispatcher = require("flux").Dispatcher;
+var React = require("react");
+
+var testDispatcher = new Dispatcher();
+
+
+// action
+var testAction = {
+  test: function (val) {
+    testDispatcher.dispatch({
+      actionType: "test",
+      value: val
+    });
+  }
+};
+
+
+// store
+var TestStore = {list: null};
+testDispatcher.register(function (payload) {
+  if (payload.actionType === "test") {
+    alert(payload.value);
+  }
+});
+
+
+// view
+var TestBox = React.createClass({
+  getInitialState() {
+    return {
+      value: ""
+    };
+  },
+  render: function () {
+    return (
+      <div className="testBox">
+        <TestForm data={this.state.value} />
+      </div>
+    );
+  }
+});
+
+var TestForm = React.createClass({
+  send: function (e) {
+    var val = React.findDOMNode(this.refs.val).value.trim();
+    testAction.test(val);
+    React.findDOMNode(this.refs.val).value = "";
+    return;
+  },
+  render: function () {
+    return (
+      <form>
+        <input type="text" ref="val" />
+        <button onClick={this.send}>送信</button>
+      </form>
+    );
+  }
+});
+
+React.render(
+  <TestBox />,
+  document.getElementById("content")
+);
